46cf5f199b453ad70b68b6ae31c87af8a6edca2c
Abdul Malik Pasha <82267578+Abdul-malik-1234@users.noreply.github.com>
2023-02-25 18:33:38 +0530

    git commit Linkedlist.cpp -m "Added some code"

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -31,15 +31,50 @@ Node* insert_at_head(Node* head,Node* n){
     head=n;
     return head;
 }
+//iterative-way
+Node* reversell(Node* head){
+    Node* prev=NULL;
+    Node* curr=head;
+    while(curr!=NULL){
+        Node* temp=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=temp;
+    }
+    return prev;
+}
+//recursive-way
+Node* reverse(Node* head){
+    if(head==NULL or head->next==NULL) return head;
+    Node* newhead=reverse(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return newhead;
+}
 int main(){
+    cout<<"Creating a head node of 5:"<<endl;
     Node* h=new Node(5);
+    print_all_nodes(h);
+    cout<<"\n";
+    cout<<"Inserting a 9,7,6 at head node of 5:"<<endl;
     for(int i=9;i>=6;i--){
         h=insert_at_tail(h,new Node(i));
     }
-    // print_all_nodes(h);
+    print_all_nodes(h);
+    cout<<"\n";
+    cout<<"Inserting a 4,3,2,1 at head node of 5 and new head becomes 1:"<<endl;
     for(int i=4;i>=1;i--){
         h=insert_at_head(h,new Node(i));
+        // break;
     }
     print_all_nodes(h);
+    cout<<"\n";
+    cout<<"Reversing the above linked list and now new head becomes 6:"<<endl;
+    Node* head=reversell(h);
+    print_all_nodes(head);
+    cout<<"\n";
+    cout<<"Reversing the above linked list recursively and now new head becomes 1:"<<endl;
+    head=reverse(head);
+    print_all_nodes(head);
     return 0;
 }
